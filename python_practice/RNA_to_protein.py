eef44109378109417d7abc6c92288543dd477007
Li Lei <lilei@lbl.gov>
2025-05-06 16:28:43 -0700

    add RNAto proten python code

---
+++ python_practice/RNA_to_protein.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import sys
+
+# Define the codon table
+codon_table = {
+    'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
+    'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
+    'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',
+    'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',
+    'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+    'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+    'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
+    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
+}
+
+# Define a function to translate RNA to protein
+def translate_rna(rna):
+    protein = ''
+    for i in range(0, len(rna), 3):
+        codon = rna[i:i+3]
+        if codon_table[codon] == '*':
+            break
+        protein += codon_table[codon]
+    return protein
+
+# Read the RNA sequence from the input file
+file = sys.argv[1]
+with open (file, 'r') as F:
+    rna = F.readline().strip()
+
+# Translate the RNA to protein and print the result
+print(translate_rna(rna))
+
